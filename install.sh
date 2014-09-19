fe147fa86d4e61c5a1f63341ddc050aa09e6c46e
Julien Rottenberg <Julien.Rottenberg@mgo.com>
2014-09-18 18:09:10 -0700

    First working version
    It backs up the file, in case the user wants to revert, but doesn't
    support rollback. If the user wants to rollback he needs to pick the
    backupped file and replace.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+HIPCHAT_APP=/Applications/HipChat.app/Contents/Resources/
+SANE_CSS=https://raw.githubusercontent.com/yagoferrer/hipchat2.0-theme/master/Contents/Resources
+
+
+NOW=$(date +%Y%m%d_%H%M)
+
+ASSETS='chat-osx.css chat.css'
+
+
+for file in ${ASSETS}; do
+	echo -e "\n\tWorking on $file"
+	mv ${HIPCHAT_APP}/${file} ${HIPCHAT_APP}/${file}_${NOW}
+	wget --quiet --output-document=${HIPCHAT_APP}/${file} ${SANE_CSS}/${file}
+	echo "\tNew version installed, backup version at ${HIPCHAT_APP}/${file}_${NOW}"
+done
+
+echo -e "\n\n\tAll done, please restart Hipchat to take advantage of the 2.0 theme\n\n"
